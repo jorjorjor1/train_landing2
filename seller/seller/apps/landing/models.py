ad882e68e14acb140604aee354b1140c32bb7203
asherstnev-s <44211081+jorjorjor1@users.noreply.github.com>
2019-11-29 13:57:25 +0300

    check

--- seller/seller/apps/landing/models.py
+++ seller/seller/apps/landing/models.py
@@ -26,7 +26,7 @@ class FlatRodonit(models.Model):
     """
     Model representing a book (but not a specific copy of a book).
     """
-    flat_name = models.CharField(max_length=200)
+    flat_name = models.CharField(max_length=300)
     price = models.CharField('Цена', max_length=20, null=True)
     pic = StdImageField(upload_to=get_image_path, blank=True, null=True, variations={'large': {'width': 1000, 'height': 500}})
     
@@ -40,7 +40,7 @@ class FlatAqvarel(models.Model):
     """
     Model representing a book (but not a specific copy of a book).
     """
-    flat_name = models.CharField(max_length=200)
+    flat_name = models.CharField(max_length=300)
     price = models.CharField('Цена', max_length=20, null=True)
     pic = StdImageField(upload_to=get_image_path, blank=True, null=True, variations={'large': {'width': 1000, 'height': 500}})
     
@@ -56,7 +56,7 @@ class InfoText(models.Model):
     h1_font_size = models.IntegerField(default=18, blank = True)
     h1_text_align = models.CharField(default='center', max_length=15, blank = True)
     h1_text_color = models.CharField(default='black', max_length=50,blank = True)
-    text_content = models.TextField(max_length=2000, blank = True)
+    text_content = models.TextField(max_length=5000, blank = True)
     text_font_size = models.IntegerField(default=18, blank = True)
     text_align = models.CharField(default='center', max_length=15, blank = True)
     text_color = models.CharField(default='black', max_length=50, blank = True)
